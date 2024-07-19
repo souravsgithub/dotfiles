42d4056458d790e475cfd2259ec7d75b06857771
sourav <souravkumarw845@gmail.com>
2024-07-20 02:48:07 +0530

    made some changes to the dotfiles

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
  return {
-  "catppuccin/nvim",
-  name = "catppuccin",
+  "lunarvim/darkplus.nvim",
+  name = "darkplus",
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme("catppuccin-mocha")
+    vim.cmd.colorscheme("darkplus")
   end
 }
--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -3,9 +3,9 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
     require('lualine').setup({
-      options = {
-        theme = "dracula",
-      },
+      -- options = {
+      --   theme = "dracula",
+      -- },
     })
   end
 }
