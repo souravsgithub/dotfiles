c43cd8295460b1349463cb59397081299f229b55
sourav <souravkumarw845@gmail.com>
2024-07-22 17:23:06 +0530

    made some chagnes that I don't know to be honest

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "clangd", "lua_ls", "tsserver", "marksman" },
+				ensure_installed = { "clangd", "lua_ls", "tsserver", "marksman", "jdtls" },
 			})
 		end,
 	},
